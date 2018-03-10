8273e1c2dd1b78a298d46cdb300f290cf3dc5e09
Raphael Caradonna <caradrap@students.zhaw.ch>
2018-03-10 10:48:50 +0100

    initial creation of database connection class.

---
+++ src/src/main/java/datenbank/DatabaseConnector.java
@@ -0,0 +1,12 @@
+package datenbank;
+
+import java.io.Console;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class DatabaseConnector {
+	f
+	
+}	
