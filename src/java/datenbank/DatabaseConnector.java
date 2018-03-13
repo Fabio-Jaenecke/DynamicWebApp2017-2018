f20dd00d561e868e64881d9d4a3dd8f05c6d3479
caradrap <caradrap@students.zhaw.ch>
2018-03-13 20:07:03 +0100

    fixed little problems.

--- src/java/datenbank/DatabaseConnector.java
+++ src/java/datenbank/DatabaseConnector.java
@@ -35,9 +35,10 @@ public class DatabaseConnector {
 	 * @return database connection
 	 */
 	private static Connection establishH2DBConnection() {
-		String DB_Connection = "jdbc:h2:~/histarantia";
+		String DB_Connection = "jdbc:h2:~/test";
+		//String DB_Connection = "jdbc:mysql://localhost:3306/";
 		String DB_Driver = "org.h2.Driver";	
-		String DB_User = "user";
+		String DB_User = "sa";
 		String DB_Password = "";
 		
 		try {
@@ -80,9 +81,10 @@ public class DatabaseConnector {
 			Statement statement;
 			statement = conn.createStatement();
 			statement.execute("drop table lebensmitteldaten if exists");
-			statement.execute("CREATE TABLE Lebensmitteldaten(lindex int(3) primary key, lname varchar(100),"
+			statement.execute("CREATE TABLE lebensmitteldaten(lindex int(3) primary key, lname varchar(100),"
 				+ " karenzphase varchar(100), dauerernaehrung varchar(100));");
 			statement.close();
+			conn.commit();
 		} catch (SQLException e) {
 			catchException(e);
 		}
@@ -96,9 +98,10 @@ public class DatabaseConnector {
 		try {
 			Statement statement = conn.createStatement();
 			statement.execute("drop table zugriffsskala if exists");
-			statement.execute("CREATE TABLE Zugriffsskala(zindex int(3) primary key, platzierung int(3),"
+			statement.execute("CREATE TABLE zugriffsskala(zindex int(3) primary key, platzierung int(3),"
 					+ " aZugriffe int(3));");
 			statement.close();
+			conn.commit();
 		} catch (SQLException e) {
 			catchException(e);
 		}
@@ -111,8 +114,9 @@ public class DatabaseConnector {
 		try {
 			Statement statement = conn.createStatement();
 			statement.execute("drop table lebensmittelkategorie if exists");
-			statement.execute("CREATE TABLE Lebensmittelkategorie(kindex int(3) primary key, kname varchar(100));");
+			statement.execute("CREATE TABLE lebensmittelkategorie(kindex int(3) primary key, kname varchar(100));");
 			statement.close();
+			conn.commit();
 		} catch (SQLException e) {
 			LOGGER.log(Level.SEVERE, " Exception occured during creation of table ", e);
 		}
@@ -125,9 +129,10 @@ public class DatabaseConnector {
 		try {
 			Statement statement = conn.createStatement();
 			statement.execute("drop table kategorieneinteilung if exists");
-			statement.execute("CREATE TABLE Kategorieneinteilung(keindex int(3) primary key, kindex1 int(3),"
+			statement.execute("CREATE TABLE kategorieneinteilung(keindex int(3) primary key, kindex1 int(3),"
 					+ " kindex2 int(3), kindex3 int(3));");
 			statement.close();
+			conn.commit();
 		} catch (SQLException e) {
 			catchException(e);
 		}
@@ -139,9 +144,10 @@ public class DatabaseConnector {
 	private void setUpTableNaehrstoff() {
 		try {
 			Statement statement = conn.createStatement();
-			statement.execute("drop table naehrstoff if exists)");
+			statement.execute("drop table naehrstoff if exists");
 			statement.execute("CREATE TABLE naehrstoff(nindex int(3) primary key, nname varchar(100));");
 			statement.close();
+			conn.commit();
 		} catch (SQLException e) {
 			catchException(e);
 		}
@@ -153,10 +159,11 @@ public class DatabaseConnector {
 	private void setUpTableFavorit() {
 		try {
 			Statement statement = conn.createStatement();
-			statement.execute("drop table favorit if exists)");
-			statement.execute("CREATE TABLE Favorit(zindex int(3), lindex int(3), primary key(zindex, lindex),"
+			statement.execute("drop table favorit if exists");
+			statement.execute("CREATE TABLE favorit(zindex int(3), lindex int(3), primary key(zindex, lindex),"
 					+ "foreign key(zindex) references zugriffsskala, foreign key(lindex) references lebensmitteldaten));");
 			statement.close();
+			conn.commit();
 		} catch (SQLException e) {
 			catchException(e);
 		}
@@ -168,11 +175,12 @@ public class DatabaseConnector {
 	private void setUpTableKatzugehoerigkeit() {
 		try {
 			Statement statement = conn.createStatement();
-			statement.execute("drop table katzugehoerigkeit if exists)");
+			statement.execute("drop table katzugehoerigkeit if exists");
 			statement.execute("CREATE TABLE katzugehoerigkeit(lindex int(3), kindex int(3),"
 					+ " primary key(kindex, lindex),foreign key(kindex) references lebensmittelkategorie"
 					+ ", foreign key(lindex) references lebensmitteldaten);");
 			statement.close();
+			conn.commit();
 		} catch (SQLException e) {
 			catchException(e);
 		}
@@ -184,11 +192,12 @@ public class DatabaseConnector {
 	private void setUpTableEnhaelt() {
 		try {
 			Statement statement = conn.createStatement();
-			statement.execute("drop table enthaelt if exists)");
-			statement.execute("CREATE TABLE Enthaelt(kindex int(3), keindex int(3), primary key(kindex, keindex),"
+			statement.execute("drop table enthaelt if exists");
+			statement.execute("CREATE TABLE enthaelt(kindex int(3), keindex int(3), primary key(kindex, keindex),"
 					+ " foreign key(kindex) references lebensmittelkategorie, "
 					+ "foreign key(keindex) references kategorieneinteilung);");
 			statement.close();
+			conn.commit();
 		} catch (SQLException e) {
 			catchException(e);
 		}
@@ -200,10 +209,11 @@ public class DatabaseConnector {
 	private void setUpTableNaehrzugehoerigkeit() {
 		try {
 			Statement statement = conn.createStatement();
-			statement.execute("drop table naehrzugehoerigkeit if exists)");
+			statement.execute("drop table naehrzugehoerigkeit if exists");
 			statement.execute("CREATE TABLE naehrzugehoerigkeit(nindex int(3), lindex(3), primary key(nindex, lindex)," 
 					+ " foreign key(nindex) references naehrstoff, foreign key(lindex) references lebensmitteldaten);");
 			statement.close();
+			conn.commit();
 		} catch (SQLException e) {
 			catchException(e);
 		}
