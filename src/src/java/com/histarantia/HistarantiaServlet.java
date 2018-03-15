64bd6c33884f8c346bea64388a3ecc9b450f02ce
Raphael Caradonna <caradrap@students.zhaw.ch>
2018-03-15 17:18:16 +0100

    added main servlet

---
+++ src/src/java/com/histarantia/HistarantiaServlet.java
@@ -0,0 +1,38 @@
+package com.histarantia.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HistarantiaServlet
+ */
+@WebServlet(description = "Main servlet for application", urlPatterns = { "/home" })
+
+public class HistarantiaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HistarantiaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String nextJSP = "/home.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+         dispatcher.forward(request, response);
+	}
+
+}
