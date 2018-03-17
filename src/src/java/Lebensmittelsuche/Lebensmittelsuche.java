b66c55fee0198895428d4e58776043df9281151e
Michèle Trebo <trebomic@students.zhaw.ch>
2018-03-17 21:25:18 +0100

    In der Klasse Lebenmittelsuche.java versucht die Klasse Lebensmittel.java zu integrieren und die Methode public void sucheLebensmittel versucht zu implementieren. // TODO: Fehler korrigieren und Ergänzungen machen gem. Pflichtenheft.

--- src/src/java/Lebensmittelsuche/Lebensmittelsuche.java
+++ src/src/java/Lebensmittelsuche/Lebensmittelsuche.java
@@ -1,11 +1,28 @@
 package Lebensmittelsuche; 
 
-import java.util.*; 
+import java.util.*;
+import Lebensmittel.Lebensmittel; 
 
-public class Lebensmittelsuche {
+public class Lebensmittelsuche extends Lebensmittel {
 	
-	private List<String> lebensmittel; 
+	private String lebensmittelname; 
+	private String lebensmittelkategorie; 
+	private String karenzphase; 
+	private String dauerernährung; 
 	
+	public Lebensmittelsuche(String lebensmittelname)
+	{
+		super();
+	}
 	
+	public void sucheLebensmittel(String lebensmittelname)
+	{
+		int index = 0; 
+		boolean gefunden = false; 
+		while(index < lebensmittel.size() && !gefunden) {
+			String lebensmittelname = lebensmittel.get(index); 
+			System.out.println( + ": " + lebensmittelname);
+			index++; 
+	}
 
 }
