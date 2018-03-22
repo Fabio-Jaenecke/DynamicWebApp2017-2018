86e46d802ba59a3701c92e27fc0b9585fb1b5841
Michèle Trebo <trebomic@students.zhaw.ch>
2018-03-22 23:25:43 +0100

    JUnit Test erstellt. TODO: Test schreiben.

---
+++ src/src/java/controller/LebensmittelmanagerTest.java
@@ -0,0 +1,26 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LebensmittelmanagerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
+
+//TODO: Tests schreiben. 
