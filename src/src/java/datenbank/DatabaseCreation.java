8110dcb38e8fa2f89c4f30d220067d8881d1c417
Marko Despotovic <despomar@students.zhaw.ch>
2018-03-29 15:30:40 +0200

    Datenbank mit Daten gefüllt und Datenbankschema rekonstrukturiert:(Tabelle Kategorieneinteilung entfernt und anstatt Tabelle Naehrstoff mit der Tabelle Enthaelt verbunden)

--- src/src/java/datenbank/DatabaseCreation.java
+++ src/src/java/datenbank/DatabaseCreation.java
@@ -10,8 +10,6 @@ public interface DatabaseCreation {
 	
 	void setUpTableLebensmittelkategorie();
 	
-	void setUpTableKategorieneinteilung();
-	
 	void setUpTableNaehrstoff();
 	
 	void setUpTableFavorit();
