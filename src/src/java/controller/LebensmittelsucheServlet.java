faafcd4155cd9b6e3f23c34dd1a0021ebeea5302
jaenefab <jaenefab@students.zhaw.ch>
2018-04-02 23:09:19 +0200

    LebensmittelsucheServlet erstellt.

---
+++ src/src/java/controller/LebensmittelsucheServlet.java
@@ -0,0 +1,42 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import datenbank.DatabaseConnector;
+
+
+/**
+* Servlet implementation class LebensmittelKategorieServlet
+*/
+@WebServlet(description = "Servlet für die Lebensmittelsuche", urlPatterns = { "/lebensmittelsuche/" })
+
+public class LebensmittelsucheServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LebensmittelsucheServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    /**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String nextJSP = "/jsp/suche.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+         dispatcher.forward(request, response);
+	}
+
+}
