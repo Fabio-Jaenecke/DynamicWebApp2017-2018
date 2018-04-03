bcb3eef2923e939d5980ddf3db1ab0143136e5fe
Michèle Trebo <trebomic@students.zhaw.ch>
2018-04-03 18:04:39 +0200

    Unnötige Methode gelöscht

--- src/src/java/Suche/Lebensmittelsuche.java
+++ src/src/java/Suche/Lebensmittelsuche.java
@@ -31,30 +31,4 @@ public abstract class Lebensmittelsuche implements Suche {
 	public Lebensmitteldaten getLebensmittelInfoByKategorie(String kategorieName, String lebensmittelName) {
 		return lebensmittelmanager.getLebensmittelInfoByKategorie(kategorieName, lebensmittelName);
 	}
-	
-<<<<<<< HEAD
-	public Lebensmitteldaten getNextLebensmittel() {
-		if (lebensmittelmanager.listIterator().hasNext() != true) {
-			getNextLebensmittel(); 
-		}
-		else {
-			next = lebensmittelmanager.listIterator().next(); 
-		}
-		return next; 
-=======
-	public void getNextLebensmittel() {
-		litr = lebensmittelmanager.lebensmittel.listIterator();
-		int i = 0;
-		while (litr.hasNext()) {
-		    lebensmittelmanager.lebensmittelAusgeben(i);
-		    i++;
-		}
->>>>>>> branch 'master' of https://github.engineering.zhaw.ch/ps-17-bacn-sckt/ps_17_gruppe23
-	}
 }
-
-<<<<<<< HEAD
-//TODO: write getNextLebensmittel-Method (caradrap)
-=======
-//TODO: review getNextLebensmittel-Method (caradrap)
->>>>>>> branch 'master' of https://github.engineering.zhaw.ch/ps-17-bacn-sckt/ps_17_gruppe23
