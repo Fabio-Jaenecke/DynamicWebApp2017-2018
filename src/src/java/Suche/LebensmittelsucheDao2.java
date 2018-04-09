52741b465fde5cdbf563ca098c93b1da1cf095ed
Michèle Trebo <trebomic@students.zhaw.ch>
2018-04-09 17:53:17 +0200

    Javadoc ergänzt #7

--- src/src/java/Suche/LebensmittelsucheDao2.java
+++ src/src/java/Suche/LebensmittelsucheDao2.java
@@ -9,16 +9,30 @@ import datenbank.connector.DbConnector;
 import datenbank.container.Lebensmitteldaten;
 import datenbank.dao.DbQuery;
 
+/**
+ * Diese Klasse uebergibt die Datenbankabfrage zur Datenbank und fuehrt eine Suche
+ * mit einem vordefinierten Lebensmitteln durch.
+ *
+ * @author Raphael Caradonna und Michele Trebo 
+ * @version 09.04.2018 
+ */
 public class LebensmittelsucheDao2 {
 
 	private Lebensmitteldaten lebensmittel;
 	DbQuery query = new DbQuery();
 	private static final Logger LOGGER = Logger.getLogger(DbConnector.class.getName());
 	
+	/**
+	 * Konstruktor für LebensmittelsucheDao. 
+	 */
 	public LebensmittelsucheDao2() {
 		
 	}
 	
+	/**
+	 * Suche nach dem Lebensmittel. 
+	 * @param lebensmittelname der Name des Lebensmittel. 
+	 */
 	public void searchForString(String lebensmittelname) {
 		String selectSQL = "select * from lebensmitteldaten where lname like '%" + lebensmittelname + "%';";
 		ResultSet result = query.getResult(selectSQL);
@@ -34,7 +48,11 @@ public class LebensmittelsucheDao2 {
 			LOGGER.log(Level.SEVERE, "resultSet could not be resolved " + e);
 		}
 	}
-
+	
+	/**
+	 * Liefere die Lebensmitteldaten des entsprechenden Lebensmittel. 
+	 * @return lebensmittel 
+	 */
 	public Lebensmitteldaten getLebensmittel() {
 		return lebensmittel;
 	}
