21d84d6d02af0bab70624e45901ddd21c38fff67
jaenefab <jaenefab@students.zhaw.ch>
2018-04-11 23:52:21 +0200

    added NaehrstoffsucheDaoTest

---
+++ src/src/java/Suche/NaehrstoffsucheDaoTest.java
@@ -0,0 +1,32 @@
+package Suche;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Testklasse fuer die Klasse KategoriesucheDao.
+ * 
+ * @author Fabio Jaenecke
+ * @version 11.04.2018
+ */
+
+//TODO: Write testclass 
+public class NaehrstoffsucheDaoTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
