62d18654eb049eb73fa4b7b70333ef3be11376d3
Michèle Trebo <trebomic@students.zhaw.ch>
2018-04-12 16:13:15 +0200

    JUnit-Test für Zugriffsmanager erstellt.

---
+++ src/src/java/zugriffe/ZugriffsskalamanagerTest.java
@@ -0,0 +1,33 @@
+package zugriffe;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Testklasse für die Klasse Zugriffsskalamanager.
+ * 
+ * @author Michele Trebo
+ * @version 12.04.2018
+ *
+ */
+
+//TODO: Write testclass
+public class ZugriffsskalamanagerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
