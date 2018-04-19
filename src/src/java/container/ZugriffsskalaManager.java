8b5baae0fd2c7b1f9d87c3ce8e22f561b7ca010d
Michèle Trebo <trebomic@students.zhaw.ch>
2018-04-19 18:26:19 +0200

    Deleted unused imports

--- src/src/java/container/ZugriffsskalaManager.java
+++ src/src/java/container/ZugriffsskalaManager.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 import java.util.logging.Level; 
 import java.util.logging.Logger; 
 
-import datenbank.connector.DbConnector;
 import datenbank.container.ZugriffsManager;
 import datenbank.dao.DbQuery; 
 
