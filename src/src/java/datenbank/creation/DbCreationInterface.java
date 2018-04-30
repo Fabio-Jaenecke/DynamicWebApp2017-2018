919deebba4d1544e8774ad855ae33979025e2748
Michèle Trebo <trebomic@students.zhaw.ch>
2018-04-30 17:08:43 +0200

    Refactoring #28

--- src/src/java/datenbank/creation/DbCreationInterface.java
+++ src/src/java/datenbank/creation/DbCreationInterface.java
@@ -1,8 +1,8 @@
 package datenbank.creation;
 
-/*
- * interface for inserting data to tables.
- * every method creates one table
+/**
+ * Interface zum Einfügen von Daten in Tabellen.
+ * Jede Methode erstellt eine Tabelle. 
  */
 public interface DbCreationInterface {
 
