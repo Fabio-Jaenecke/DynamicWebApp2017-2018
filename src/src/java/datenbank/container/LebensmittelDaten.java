5f053d26749a5368aba8d23c745adb94701cfb28
Michèle Trebo <trebomic@students.zhaw.ch>
2018-04-30 16:33:44 +0200

    Refactoring LebensmittelDaten.java #28

--- src/src/java/datenbank/container/LebensmittelDaten.java
+++ src/src/java/datenbank/container/LebensmittelDaten.java
@@ -8,15 +8,13 @@ import java.util.logging.Logger;
 import datenbank.connector.DbConnector;
 
 /**
- * stellt die Java-Klasse zur Entitaet lebensmittelDaten
- * in der Datenbank da
- * @author Raphael Caradonna
- *
+ * Stellt die Java-Klasse zur Entitaet lebensmittelDaten
+ * in der Datenbank dar. 
  */
 public class LebensmittelDaten {
 	
 	/**
-	 * Attribute aus der Entitaet lebensmittelDaten
+	 * Attribute aus der Entitaet lebensmittelDaten.
 	 */
 	private int index;
 	private String lebensmittelname;
@@ -26,7 +24,7 @@ public class LebensmittelDaten {
 	private static final Logger LOGGER = Logger.getLogger(DbConnector.class.getName());
 	
 	/**
-	 * Standardkonstruktor fuer ein Lebensmittel
+	 * Standardkonstruktor fuer ein Lebensmittel.
 	 * @param index der Index des Lebensmittels
 	 * @param name der Name des Lebensmittels
 	 * @param karenzphase der erste Toleranzwert
@@ -41,7 +39,7 @@ public class LebensmittelDaten {
 	}
 		
 	/**
-	 * Konstruktor fuer ein Tupel aus der Datenbank
+	 * Konstruktor fuer ein Tupel aus der Datenbank. 
 	 * @param rs ein Lebensmitteltupel
 	 * @throws SQLException SQL-Exception
 	 */
@@ -57,53 +55,92 @@ public class LebensmittelDaten {
 		}
     }
 
+	/**
+	 * Get the Lindex. 
+	 * @return index
+	 */
 	public int getLindex() {
 		return index;
 	}
 
+	/**
+	 * Set the Lindex. 
+	 * @param lindex
+	 */
 	public void setLindex(int lindex) {
 		this.index = lindex;
 	}
 
+	/**
+	 * Get the Lname.
+	 * @return lebensmittelname
+	 */
 	public String getLname() {
 		return lebensmittelname;
 	}
 
+	/**
+	 * Set the Lname. 
+	 * @param lname
+	 */
 	public void setLname(String lname) {
 		this.lebensmittelname = lname;
 	}
 
+	/**
+	 * Get the Karenzphase. 
+	 * @return karenzphase
+	 */
 	public String getKarenzphase() {
 		return karenzphase;
 	}
 
+	/**
+	 * Set the Karenzphase.
+	 * @param karenzphase
+	 */
 	public void setKarenzphase(String karenzphase) {
 		this.karenzphase = karenzphase;
 	}
 
+	/**
+	 * Get the Dauerernaehrung. 
+	 * @return dauerernaehrung
+	 */
 	public String getDauerernaehrung() {
 		return dauerernaehrung;
 	}
 
+	/**
+	 * Set the Dauerernaehrung. 
+	 * @param dauerernaehrung
+	 */
 	public void setDauerernaehrung(String dauerernaehrung) {
 		this.dauerernaehrung = dauerernaehrung;
 	}
 	
+	/**
+	 * Get the Kategorie. 
+	 * @return kategorie
+	 */
 	public String getKategorie() {
 		return kategorie;
 	}
 
+	/**
+	 * Set the Kategorie. 
+	 * @param kategorie
+	 */
 	public void setKategorie(String kategorie) {
 		this.kategorie = kategorie;
 	}
 
 	/**
-	 * gibt Datenfelder des Lebensmittels als String zurueck
+	 * Gibt die Datenfelder des Lebensmittels als String zurueck.
 	 * @override
 	 */
 	public String toString() {
 		return getLindex() + String.format(" ", getLname(), " ", 
 				getKarenzphase(), " ", getDauerernaehrung(), " ", getKategorie());
 	}
-
 }
