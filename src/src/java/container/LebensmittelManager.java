b5f806dbbf4e6fbfb09af59b621caaea5d873b12
jaenefab <jaenefab@students.zhaw.ch>
2018-05-04 03:23:16 +0200

    added variables to abstract class LebensmittelManager

--- src/src/java/container/LebensmittelManager.java
+++ src/src/java/container/LebensmittelManager.java
@@ -10,6 +10,11 @@ public abstract class LebensmittelManager {
   protected String karenzphase;
   protected String dauerernaehrung;
   protected String kategorie;
+  protected int zindex;
+  protected int platzierung;
+  protected int azugriffe;
+  protected int kindex;
+  protected String kname;
   
   public abstract boolean isNil();
   
